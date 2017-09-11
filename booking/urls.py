1ef743826f3c37f31e942be3b5cdf02df523e2f4
Kunal Pandav <kunal.pandav@umctec.com>
2017-09-11 19:18:04 +0530

    services_page_done

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^test$', views.test, name='test'),
     url(r'^index$', views.index, name='index'),
     url(r'^about$', views.about, name='about'),
+    url(r'^services$', views.services, name='services'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -17,3 +17,6 @@ def index(request):
 
 def about(request):
     return render(request,"about.html")
+
+def services(request):
+    return render(request,"services.html")
