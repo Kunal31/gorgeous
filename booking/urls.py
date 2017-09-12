3e2989d6ce3040ebc6982396b672d2b3c35309e3
Kunal Pandav <kunal.pandav@umctec.com>
2017-09-12 15:33:46 +0530

    template_inheritance_done

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     url(r'^index$', views.index, name='index'),
     url(r'^about$', views.about, name='about'),
     url(r'^services$', views.services, name='services'),
+    url(r'^portfolio$', views.portfolio, name='portfolio'),
+
+    url(r'^blog_grid$',views.blog_grid, name='blog_grid'),
+    url(r'^blog_single', views.blog_single, name='blog_single'),
+    url(r'^blog_details$',views.blog_details, name='blog_details'),
 ]
--- booking/views.py
+++ booking/views.py
@@ -20,3 +20,15 @@ def about(request):
 
 def services(request):
     return render(request,"services.html")
+
+def portfolio(request):
+    return render(request,"portfolio-1.html")
+
+def blog_grid(request):
+    return render(request,"blog-grid.html")
+
+def blog_single(request):
+    return render(request,"blog-single-post.html")
+
+def blog_details(request):
+    return render(request,"blog-details.html")
